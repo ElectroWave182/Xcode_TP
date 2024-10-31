99112a3d5dd5b2c0412d443619726abd1fd8b2da
Enzo C <150686082+ElectroWave182@users.noreply.github.com>
2024-11-01 00:54:35 +0100

    Add files via upload

---
+++ JeuViewController.swift
@@ -0,0 +1,34 @@
+//
+//  JeuViewController.swift
+//  Tri
+//
+//  Created by Enzo Causse on 05/03/2024.
+//
+
+import UIKit
+
+class JeuViewController: UIViewController
+{
+
+    @IBOutlet var dechets: [UIImageView]!
+    @IBOutlet var poubelles: [UIImageView]!
+    
+    override func viewDidLoad ()
+    {
+        super.viewDidLoad ()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
